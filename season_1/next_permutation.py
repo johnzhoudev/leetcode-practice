3210a5fb30121dea1063a61207303ef04d5ccd5e
John Zhou <john2001@live.ca>
2022-07-30 11:14:57 -0400

    Added faster O(n) solution to next_permutation

--- season_1/next_permutation.py
+++ season_1/next_permutation.py
@@ -1,4 +1,70 @@
 # Results:
+# Runtime: 50ms 83.28%
+# Memory Usage: 13.9MB 25.58%
+
+"""
+Idea: O(n) time, O(1) space
+
+1 3 4 2
+
+1. Find the swap position: search from right and find first one in descending order (smaller than last)
+2. Find item to swap: the smallest item that's larger than the swap position
+3. Swap
+4. Reverse the rest of the thing
+
+Why it works:
+1. To get next one, we need to move a bigger number closer to the left. Rest is unchanged.
+- the very next one, once we move the bigger number, has to be in ascending order in order to be the very next one
+2. So at the swap position, everything to the right must be in descending order since if it was ascending you could swap earlier
+- so we can find the swap by looking for the first thing that's descending from the right
+- then, once we swap with the smallest number we can swap with, the position of the thing swapped out will actually be 
+	in the correct descending order since to the left, larger, and to the right, must be smaller else would have been swapped
+3. Then just reverse the end
+"""
+
+def swap(nums, i, j):
+	temp = nums[i]
+	nums[i] = nums[j]
+	nums[j] = temp
+
+def reverse(nums, start): # reverses to end
+	end = len(nums) - 1
+	while (start < end):
+		swap(nums, start, end)
+		start += 1
+		end -= 1
+	
+def nextPermutation2(nums):
+	# 1. find swap position
+
+	swapPosition = -1 # -1 for edge case, never find swap position since all ascending from right to left.
+	last = float("-inf")
+	for i in range(len(nums) - 1, -1, -1):
+		if (last > nums[i]): # i is the swap
+			swapPosition = i
+			break
+		last = nums[i]
+
+	# handle edge case of no swap
+	if (swapPosition == -1):
+		reverse(nums, 0)
+		return
+
+	# 2. find the item to swap
+	indexToSwap = swapPosition + 1 # largets element right of swap position, leftmost elt if match
+	# for i in range(len(nums) - 1, swapPosition, -1):
+	for i in range(swapPosition + 1, len(nums)):
+		# same value, so set to further one to maintain descending order
+		if ((nums[i] == nums[indexToSwap]) or (nums[swapPosition] < nums[i] and nums[i] < nums[indexToSwap])):
+			indexToSwap = i
+	
+	# 3. swap 4. reverse
+	swap(nums, swapPosition, indexToSwap)
+	reverse(nums, swapPosition + 1)
+
+# Old Version
+
+# Results:
 # Runtime: 75ms 32.47%
 # Memory Usage: 13.8MB 74.94%
 
@@ -54,10 +120,10 @@ Questions:
 - Duplicates?
 """
 
-def swap(nums, i, j):
-	temp = nums[i]
-	nums[i] = nums[j]
-	nums[j] = temp
+# def swap(nums, i, j):
+# 	temp = nums[i]
+# 	nums[i] = nums[j]
+# 	nums[j] = temp
 
 # Sorts nums in place from index [i:]
 # O(n^2) just find the min and swap
@@ -98,7 +164,7 @@ def nextPermutation(nums):
 	sortRest(nums, 0)
 
 def validate(nums, expected=None):
-	nextPermutation(nums)
+	nextPermutation2(nums)
 	if (expected != nums):
 		print("Expected: ")
 		print(expected)
