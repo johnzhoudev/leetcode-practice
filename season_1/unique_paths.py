ac74ee132e9cebbfdb5fed5067b25d0ad036ef70
John Zhou <john2001@live.ca>
2022-08-25 16:00:00 -0400

    Added recursive soln to unique_paths.py

--- season_1/unique_paths.py
+++ season_1/unique_paths.py
@@ -28,8 +28,32 @@ O(m * n) time?
 - Can do this, store indices i and j, if go left or right, add to index. check out of bounds,
 but also can do dp so that if you reach the same square, 
 
-Idea 
+Idea: DP / backtracking
+- D[i, j] = num ways to reach i, j = D[i-1, j] + D[i, j-1]
+- Or recursive search, if i, j is end, return 1. else return numPaths(left) + numPaths(down)
+- if out of bounds, return 0
+- also store D[i, j]
+Time: all possible paths, basically. may need to recalculate if not stored.
+- each square processed exactly once, so O(m x n)
+
+Idea 2: DP but D[i, j] is num paths to i, j
+Time: O(m x n)
 """
+
+def numPathsToEndFrom(m, n, currM, currN, state):
+	if (currM == m-1 and currN == n-1):
+		state[currM][currN] = 1
+	elif (currM >= m or currN >= n):
+		return 0
+	elif(state[currM][currN] is None):
+		state[currM][currN] = numPathsToEndFrom(m, n, currM + 1, currN, state) + numPathsToEndFrom(m, n, currM, currN + 1, state)
+
+	return state[currM][currN] # already solved this square
+
+def backtrackingSoln(m, n):
+	state = [[None for _ in range(n)] for _ in range(m)]
+	return numPathsToEndFrom(m, n, 0, 0, state)
+
 
 def closedSolution(m, n):
 	# just do the calculation
@@ -51,11 +75,12 @@ def closedSolution(m, n):
 	return int(top / bottom)
 
 def validate(m, n, expected):
-	if (closedSolution(m, n) != expected):
+	soln = backtrackingSoln(m, n)
+	if (soln != expected):
 		print("Error")
 		print(m)
 		print(n)
-		print("Result " + str(closedSolution(m, n)))
+		print("Result " + str(soln))
 		print("Expected " + str(expected))
 
 validate(3, 2, 3)
