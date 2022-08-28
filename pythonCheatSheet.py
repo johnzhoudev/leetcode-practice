c7b3f7ce474d471e6c0fb105ee7ea5b76822ff37
John Zhou <john2001@live.ca>
2022-08-28 16:44:49 -0400

    Added python cheat sheet

---
+++ pythonCheatSheet.py
@@ -0,0 +1,27 @@
+# Cheat sheet for python syntax
+
+# Stacks using lists
+stack = ["item1", "item2", "item3"]
+stack.append("item4")
+print(stack.pop())
+
+# Queue using deque
+from collections import deque
+deque1 = deque()
+deque1.append("item")
+deque1.appendleft("nah")
+print(deque1)
+deque1.pop()
+deque1.popleft()
+
+# queue using list
+queue1 = ["1", "2"]
+queue1.append("3")
+print(queue1.pop(0))
+
+# Array extend and append
+arr = [1, 2, 3]
+arr += [4]
+arr.extend([4, 5, 6])
+arr.append(4)
+print(arr)
