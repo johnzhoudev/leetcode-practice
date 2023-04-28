6d0218bd53126e30b10d895341db6b94f293f3dc
John Zhou <john.zhou@faire.com>
2023-04-28 16:03:00 -0400

    WIP word ladder

--- season_2_neetcode_150/word_ladder.py
+++ season_2_neetcode_150/word_ladder.py
@@ -30,7 +30,9 @@ So O(max len * n) to check all chars and make a graph, then O(n^2) dp with memoi
 """
 
 def solve(begin, end, words):
-
+    # add word begin
+    if begin not in words:
+        words.append(begin)
 
     # 0 for match, 1 for only 1, -1 for more
     def diff(s1, s2):
@@ -41,22 +43,49 @@ def solve(begin, end, words):
         return diffCount if diffCount < 2 else -1
 
     # first, build graph
-    adj = [[False for _ in range(len(words))] for _ in range(len(words))]
-    for s1Idx, s1 in enumerate(words):
-        for s2Idx, s2 in enumerate(words):
+    # adj = [[False for _ in range(len(words))] for _ in range(len(words))]
+    adj = {}
+    for word in words:
+        adj[word] = {}
+
+    for s1 in enumerate(words):
+        for s2 in enumerate(words):
             dif = diff(s1, s2)
             if dif == 0 or dif == 1:
-                adj[s1Idx][s2Idx] = True
+                adj[s1][s2] = True
+    
+    # do dijkstra's alg
+    # reachable is hash from word to num steps to it
+    reachable = {
+        begin: 0
+    }
+    min_distances = {}
+    for word in words:
+        
+
+    while reachable:
+        minSteps = len(words) + 10
+        minWord = ""
+        for word in reachable:
+            if reachable[word] < minSteps:
+                minWord = word
+                minSteps = reachable[word]
+        
+        assert minWord != ""
+        # now have min
+        min_distances
+
+
+        
+        
+
+    
     
     # setup state
     # min dist between x, y
-    state = [[-1 for _ in range(len(words))] for _ in range(len(words))]
-    for x in range(len(words)):
-        state[x][x] = 0
-    
-    # do dfs
-    visited = set()
-    def dfs(i, )
+    # state = [[-1 for _ in range(len(words))] for _ in range(len(words))]
+    # for x in range(len(words)):
+    #     state[x][x] = 0
     
 
     
