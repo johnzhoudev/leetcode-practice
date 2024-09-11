90533240527000d7d3522b138ccedb32be116b9c
John Zhou <john2001@live.ca>
2024-09-10 20:14:52 -0400

    Added pacific atlantic water flow

--- season_4_neetcode_150_again/417. Pacific Atlantic Water Flow.py
+++ season_4_neetcode_150_again/417. Pacific Atlantic Water Flow.py
@@ -19,30 +19,12 @@ O(n) time, only ever visit each block once and results in blocked or good
 Ohh, must be able to flow to both! not just one or the other
 - soln, do 2 dfs's from the corners
 
+
+Tactic: DFS or BFS from ocean to see what is reachable. DFS twice from atlantic and pacific and compare after
+
 """
 
 def solve(heights):
-
-    state = []
-    for r in range(len(heights)):
-        state.append((r, 0)) # left edge
-        # state.append((r, len(heights[0]) - 1)) # right edge
-    for c in range(len(heights[0])):
-        state.append((0, c)) # top edge
-        # state.append((len(heights) - 1, c)) # bot edge
-
-    reachableFromPacific = dfs(heights, state)
-
-    # atlantic
-    state = []
-    for r in range(len(heights)):
-        state.append((r, 0)) # left edge
-        # state.append((r, len(heights[0]) - 1)) # right edge
-    for c in range(len(heights[0])):
-        state.append((0, c)) # top edge
-        # state.append((len(heights) - 1, c)) # bot edge
-    
-    reachableFromAtlantic = dfs(heights, state)
 
     def dfs(heights, state):
         added = set()
@@ -57,11 +39,28 @@ def solve(heights):
             added.add((x, y)) # can reach
             for dx, dy in directions:
                 if not isInBounds(x + dx, y + dy): continue
-                if heights[x][y] > heights[x + dx][y + dy]: continue # can't go down
+                if heights[x][y] > heights[x + dx][y + dy]: continue # water flows down, so reverse search breaks when going up
                 state.append((x + dx, y + dy))
             
         return added
+
+    state = []
+    for r in range(len(heights)):
+        state.append((r, 0)) # left edge
+    for c in range(len(heights[0])):
+        state.append((0, c)) # top edge
+
+    reachableFromPacific = dfs(heights, state)
+
+    # atlantic
+    state = []
+    for r in range(len(heights)):
+        state.append((r, len(heights[0]) - 1)) # right edge
+    for c in range(len(heights[0])):
+        state.append((len(heights) - 1, c)) # bot edge
     
+    reachableFromAtlantic = dfs(heights, state)
+
     output = []
     for p in reachableFromAtlantic:
         if p in reachableFromPacific:
@@ -70,3 +69,4 @@ def solve(heights):
     return output
 
 
+solve([[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]])
